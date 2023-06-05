8ffc4ddebe55d0c2a2ecd6f280713141ac5575b6
meetradadiya3 <132879853+meetradadiya3@users.noreply.github.com>
2023-06-05 17:38:43 +0530

    Add files via upload

---
+++ str1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+main()
+{
+	char str[50];
+	int i,len,a=0;
+	
+	printf("Enter the word = ");
+	gets(str);
+	len=strlen(str);
+	
+		for(i=0;i<len;i++)
+		{
+			if(str[i]==str[len-i-1])
+			{
+       a=1;
+    } 
+		}
+		 	if(a==0)
+				printf("strint is palindrome");
+			else
+		 		printf("strint is not palindrome");
+}
+    
